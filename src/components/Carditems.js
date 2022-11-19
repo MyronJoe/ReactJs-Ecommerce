bf07deb77afc94f42179456545eda7c17c8d3380
Myron <josephonyekachi77@gmail.com>
2022-11-18 23:08:01 -0800

    updated

--- src/components/Carditems.js
+++ src/components/Carditems.js
@@ -1,30 +1,30 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 
 function Carditems(props) {
-    const[click, setClick] = useState(false)
+    const [click, setClick] = useState(false)
 
     const addColor = () => setClick(!click)
-  return (
-    <>
-        <li className='card-items'>
-            <div className='card'>
-            
-                <div className='img-section'>
-                    <img src={props.src}  alt={props.title} className='card-img'/>
-                    <p className='favourite-icon'><i onClick={addColor} className={click ? 'fa fa-heart favourite-icon-active' : 'fa fa-heart'}></i></p>
-                </div>
-                <Link to={props.path} >
-                    <p className='phone-name'>{props.title}</p>
-                    <div className='price-section'>
-                        <p className='price'>${props.price}</p>
-                        <p className='plus-icon'><i className='fa fa-plus'></i></p>
+    return (
+        <>
+            <li className='card-items'>
+                <div className='card'>
+
+                    <div className='img-section'>
+                        <img src={props.src} alt={props.title} className='card-img' />
+                        <p className='favourite-icon'><i onClick={addColor} className={click ? 'fa fa-heart favourite-icon-active' : 'fa fa-heart'}></i></p>
                     </div>
-                </Link>
-            </div>
-        </li>
-    </>
-  )
+                    <Link to={props.path} >
+                        <p className='phone-name'>{props.title}</p>
+                        <div className='price-section'>
+                            <p className='price'>${props.price}</p>
+                            <p className='plus-icon'><i className='fa fa-plus'></i></p>
+                        </div>
+                    </Link>
+                </div>
+            </li>
+        </>
+    )
 }
 
 export default Carditems
